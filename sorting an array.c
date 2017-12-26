30edaca7e98697018340304ef54ea148e78e69d8
Mohanad Darwish <mohanad.ahmed.yehia@gmail.com>
2017-12-26 11:03:04 +0200

    timing for the bubble sort only

--- sorting an array.c
+++ sorting an array.c
@@ -16,17 +16,16 @@ int main()
 {
     clock_t start, end;
     double cpu_time_used;
-    start = clock();
-///////////////////////////////////////////////////////////// 
     
     printf("Hello World\n");
     char i=0;
     char arr[7]={3,2,2,4,9,7,1};
+    start = clock();
     bubblesort(arr,sizeof arr);
+    end = clock();
     while(i<7) printf("arr[%d] = %d \n",i,arr[i++]);
 
-////////////////////////////////////////////////////////////
-    end = clock();
+
     cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
     printf(" time = %u",cpu_time_used);
     return 0;
